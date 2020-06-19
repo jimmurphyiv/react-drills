8e4c1410c01889d36029ab72a6b00896e4be0570
jimmurphyiv <jim.murphyiv@gmail.com>
2020-06-18 22:35:08 -0400

    progress

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Image from ',/Component/Im'
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> className="App">
+        <image url={"https:www.pexels.com/photo/silhouette-of-mountains-1323550"} />
     </div>
   );
 }

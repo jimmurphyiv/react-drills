ca1ae1c0993a94883993aea8b0669da544b32b23
jimmurphyiv <jim.murphyiv@gmail.com>
2020-07-16 19:01:03 -0400

    done

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      deathStar: ''
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://swapi.dev/api/starships/9/").then(response => {
+      this.setState({
+        lukeSkywalker: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Name: {this.state.deathStar.name}</h1>
+        <h1>Model: {this.state.deathStar.model}</h1>
+        <h1>Length: {this.state.deathStar.length}</h1>
+        <h1>Manufacture: {this.state.deathStar.manufacture}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
